a3cf23132633a72e7ae99d910466db5fcc9534e9
AgustinSRG <agustinsanromanguzman@gmail.com>
2023-04-05 12:02:59 +0200

    Update template + Tests

--- src/driver.ts
+++ src/driver.ts
@@ -2,7 +2,54 @@
 
 "use strict";
 
-import { DataSourceDriver, DataSource, GenericKeyValue, GenericRow, SortDirection, GenericFilter } from "tsbean-orm";
+import { DataSourceDriver, DataSource, GenericKeyValue, GenericRow, SortDirection, GenericFilter, GenericRowUpdate } from "tsbean-orm";
+
+function matchesFilter(row: any, filter: GenericFilter): boolean {
+    if (!filter) {
+        return true;
+    }
+
+    switch (filter.operation) {
+    case "and":
+        for (const subCondition of filter.children) {
+            if (!matchesFilter(row, subCondition)) {
+                return false;
+            }
+        }
+        return true;
+    case "or":
+        for (const subCondition of filter.children) {
+            if (matchesFilter(row, subCondition)) {
+                return true;
+            }
+        }
+        return false;
+    case "not":
+        return !matchesFilter(row, filter.child);
+    case "regex":
+        return filter.regexp.test(row[filter.key]);
+    case "in":
+        return filter.values.includes(row[filter.key]);
+    case "exists":
+        if (filter.exists) {
+            return row[filter.key] !== null && row[filter.key] !== undefined;
+        } else {
+            return row[filter.key] === null || row[filter.key] === undefined;
+        }
+    case "eq":
+        return row[filter.key] === filter.value;
+    case "ne":
+        return row[filter.key] !== filter.value;
+    case "gt":
+        return row[filter.key] > filter.value;
+    case "lt":
+        return row[filter.key] < filter.value;
+    case "gte":
+        return row[filter.key] >= filter.value;
+    case "lte":
+        return row[filter.key] <= filter.value;
+    }
+}
 
 /**
  * Driver class
@@ -19,10 +66,17 @@ export class TemplateDriver implements DataSourceDriver {
         return new DataSource("tsbean.driver.template", driver);
     }
 
-    // private connection: Connection;
+    private data: Map<string, any[]>;
 
     constructor(/* Pass here the connection object */) {
-        // this.connection = connection;
+        this.data = new Map();
+    }
+
+    private getTable(table: string): any[] {
+        if (!this.data.has(table)) {
+            this.data.set(table, []);
+        }
+        return <any[]>this.data.get(table);
     }
 
     /**
@@ -31,8 +85,12 @@ export class TemplateDriver implements DataSourceDriver {
      * @param keyName Name of the key
      * @param keyValue Value of the key
      */
-    findByKey(table: string, keyName: string, keyValue: any): Promise<GenericRow> {
-        throw new Error("Method not implemented.");
+    public async findByKey(table: string, keyName: string, keyValue: any): Promise<GenericRow> {
+        const data = this.getTable(table);
+        const filteredData = data.filter(d => {
+            return d[keyName] === keyValue;
+        });
+        return filteredData[0] || null;
     }
 
     /**
@@ -45,8 +103,56 @@ export class TemplateDriver implements DataSourceDriver {
      * @param limit Limit of results. Leave as -1 for no limit
      * @param projection List of fields to featch from the table. Leave as null to fetch them all.
      */
-    find(table: string, filter: GenericFilter, sortBy: string, sortDir: SortDirection, skip: number, limit: number, projection: Set<string>): Promise<GenericRow[]> {
-        throw new Error("Method not implemented.");
+    public async find(table: string, filter: GenericFilter, sortBy: string, sortDir: SortDirection, skip: number, limit: number, projection: Set<string>): Promise<GenericRow[]> {
+        const data = this.getTable(table);
+
+        let filteredData = data.filter(d => {
+            return matchesFilter(d, filter);
+        });
+
+        if (sortBy) {
+            filteredData = filteredData.sort((a, b) => {
+                if (sortDir === "desc") {
+                    if (a[sortBy] < b[sortBy]) {
+                        return 1;
+                    } else if (a[sortBy] > b[sortBy]) {
+                        return -1;
+                    } else {
+                        return 0;
+                    }
+                } else {
+                    if (a[sortBy] < b[sortBy]) {
+                        return -1;
+                    } else if (a[sortBy] > b[sortBy]) {
+                        return 1;
+                    } else {
+                        return 0;
+                    }
+                }
+            });
+        }
+
+        if (typeof skip === "number" && skip > 0) {
+            filteredData = filteredData.slice(skip);
+        }
+
+        if (typeof limit === "number" && limit >= 0) {
+            filteredData = filteredData.slice(0, limit);
+        }
+
+        filteredData = filteredData.map(d => {
+            const row = Object.create(null);
+
+            for (const key of Object.keys(d)) {
+                if (!projection || projection.has(key)) {
+                    row[key] = d[key];
+                }
+            }
+
+            return row;
+        });
+
+        return filteredData;
     }
 
     /**
@@ -54,8 +160,14 @@ export class TemplateDriver implements DataSourceDriver {
      * @param table Table or collection name
      * @param filter Filter to apply
      */
-    count(table: string, filter: GenericFilter): Promise<number> {
-        throw new Error("Method not implemented.");
+    public async count(table: string, filter: GenericFilter): Promise<number> {
+        const data = this.getTable(table);
+
+        const filteredData = data.filter(d => {
+            return matchesFilter(d, filter);
+        });
+
+        return filteredData.length;
     }
 
     /**
@@ -69,8 +181,58 @@ export class TemplateDriver implements DataSourceDriver {
      * @param projection List of fields to featch from the table. Leave as null to fetch them all.
      * @param each Function to parse each row
      */
-    findStream(table: string, filter: GenericFilter, sortBy: string, sortDir: SortDirection, skip: number, limit: number, projection: Set<string>, each: (row: GenericRow) => Promise<void>): Promise<void> {
-        throw new Error("Method not implemented.");
+    public async findStream(table: string, filter: GenericFilter, sortBy: string, sortDir: SortDirection, skip: number, limit: number, projection: Set<string>, each: (row: GenericRow) => Promise<void>): Promise<void> {
+        const data = this.getTable(table);
+
+        let filteredData = data.filter(d => {
+            return matchesFilter(d, filter);
+        });
+
+        if (sortBy) {
+            filteredData = filteredData.sort((a, b) => {
+                if (sortDir === "desc") {
+                    if (a[sortBy] < b[sortBy]) {
+                        return 1;
+                    } else if (a[sortBy] > b[sortBy]) {
+                        return -1;
+                    } else {
+                        return 0;
+                    }
+                } else {
+                    if (a[sortBy] < b[sortBy]) {
+                        return -1;
+                    } else if (a[sortBy] > b[sortBy]) {
+                        return 1;
+                    } else {
+                        return 0;
+                    }
+                }
+            });
+        }
+
+        if (typeof skip === "number" && skip > 0) {
+            filteredData = filteredData.slice(skip);
+        }
+
+        if (typeof limit === "number" && limit >= 0) {
+            filteredData = filteredData.slice(0, limit);
+        }
+
+        filteredData = filteredData.map(d => {
+            const row = Object.create(null);
+
+            for (const key of Object.keys(d)) {
+                if (!projection || projection.has(key)) {
+                    row[key] = d[key];
+                }
+            }
+
+            return row;
+        });
+
+        for (const row of filteredData) {
+            await each(row);
+        }
     }
 
 
@@ -85,8 +247,56 @@ export class TemplateDriver implements DataSourceDriver {
      * @param projection List of fields to featch from the table. Leave as null to fetch them all.
      * @param each Function to parse each row
      */
-    findStreamSync(table: string, filter: GenericFilter, sortBy: string, sortDir: SortDirection, skip: number, limit: number, projection: Set<string>, each: (row: any) => void): Promise<void> {
-        throw new Error("Method not implemented.");
+    public async findStreamSync(table: string, filter: GenericFilter, sortBy: string, sortDir: SortDirection, skip: number, limit: number, projection: Set<string>, each: (row: any) => void): Promise<void> {
+        const data = this.getTable(table);
+
+        let filteredData = data.filter(d => {
+            return matchesFilter(d, filter);
+        });
+
+        if (sortBy) {
+            filteredData = filteredData.sort((a, b) => {
+                if (sortDir === "desc") {
+                    if (a[sortBy] < b[sortBy]) {
+                        return 1;
+                    } else if (a[sortBy] > b[sortBy]) {
+                        return -1;
+                    } else {
+                        return 0;
+                    }
+                } else {
+                    if (a[sortBy] < b[sortBy]) {
+                        return -1;
+                    } else if (a[sortBy] > b[sortBy]) {
+                        return 1;
+                    } else {
+                        return 0;
+                    }
+                }
+            });
+        }
+
+        if (typeof skip === "number" && skip > 0) {
+            filteredData = filteredData.slice(skip);
+        }
+
+        if (typeof limit === "number" && limit >= 0) {
+            filteredData = filteredData.slice(0, limit);
+        }
+
+        filteredData = filteredData.map(d => {
+            const row = Object.create(null);
+
+            for (const key of Object.keys(d)) {
+                if (!projection || projection.has(key)) {
+                    row[key] = d[key];
+                }
+            }
+
+            return row;
+        });
+
+        filteredData.forEach(each);
     }
 
     /**
@@ -96,8 +306,14 @@ export class TemplateDriver implements DataSourceDriver {
      * @param key The name of the primary key (if any)
      * @param callback Callback to set the value of the primary key after inserting (Optional, only if auto-generated key)
      */
-    insert(table: string, row: GenericRow, key: string, callback?: (value: GenericKeyValue) => void): Promise<void> {
-        throw new Error("Method not implemented.");
+    public async insert(table: string, row: GenericRow, key: string, callback?: (value: GenericKeyValue) => void): Promise<void> {
+        const data = this.getTable(table);
+
+        data.push(row);
+
+        if (callback) {
+            callback(row[key]);
+        }
     }
 
     /**
@@ -105,8 +321,11 @@ export class TemplateDriver implements DataSourceDriver {
      * @param table Table or collection name
      * @param rows List of rows to insert
      */
-    batchInsert(table: string, rows: GenericRow[]): Promise<void> {
-        throw new Error("Method not implemented.");
+    public async batchInsert(table: string, rows: GenericRow[]): Promise<void> {
+        const data = this.getTable(table);
+        rows.forEach(row => {
+            data.push(row);
+        });
     }
 
     /**
@@ -116,8 +335,17 @@ export class TemplateDriver implements DataSourceDriver {
      * @param keyValue Value of the key
      * @param updated Updated row
      */
-    update(table: string, keyName: string, keyValue: GenericKeyValue, updated: GenericRow): Promise<void> {
-        throw new Error("Method not implemented.");
+    public async update(table: string, keyName: string, keyValue: GenericKeyValue, updated: GenericRowUpdate): Promise<void> {
+        const data = this.getTable(table);
+        const filteredData = data.filter(d => {
+            return d[keyName] === keyValue;
+        });
+
+        filteredData.forEach(d => {
+            for (const key of Object.keys(updated)) {
+                d[key] = updated[key];
+            }
+        });
     }
 
     /**
@@ -127,8 +355,26 @@ export class TemplateDriver implements DataSourceDriver {
      * @param updated Updated row
      * @returns The number of affected rows
      */
-    updateMany(table: string, filter: GenericFilter, updated: GenericRow): Promise<number> {
-        throw new Error("Method not implemented.");
+    public async updateMany(table: string, filter: GenericFilter, updated: GenericRowUpdate): Promise<number> {
+        const data = this.getTable(table);
+
+        const filteredData = data.filter(d => {
+            return matchesFilter(d, filter);
+        });
+
+        filteredData.forEach(d => {
+            for (const key of Object.keys(updated)) {
+                if (typeof updated[key] === "object" && updated[key] !== null && updated[key].update === "set") {
+                    d[key] = updated[key].value;
+                } else if (typeof updated[key] === "object" && updated[key] !== null && updated[key].update === "inc") {
+                    d[key] += updated[key].value;
+                } else {
+                    d[key] = updated[key];
+                }
+            }
+        });
+
+        return filteredData.length;
     }
 
     /**
@@ -138,18 +384,42 @@ export class TemplateDriver implements DataSourceDriver {
      * @param keyValue Value of the key
      * @returns true if the row was deleted, false if the row didn't exists
      */
-    delete(table: string, keyName: string, keyValue: GenericKeyValue): Promise<boolean> {
-        throw new Error("Method not implemented.");
+    public async delete(table: string, keyName: string, keyValue: GenericKeyValue): Promise<boolean> {
+        const data = this.getTable(table);
+
+        const toDelete = data.filter(d => {
+            return d[keyName] === keyValue;
+        }).length;
+
+        const filteredData = data.filter(d => {
+            return d[keyName] !== keyValue;
+        });
+
+        this.data.set(table, filteredData);
+
+        return toDelete > 0;
     }
-    
+
     /**
      * Deletes many rows
      * @param table Table or collection name
      * @param filter Filter to apply
      * @returns The number of affected rows
      */
-    deleteMany(table: string, filter: GenericFilter): Promise<number> {
-        throw new Error("Method not implemented.");
+    public async deleteMany(table: string, filter: GenericFilter): Promise<number> {
+        const data = this.getTable(table);
+
+        const toDelete = data.filter(d => {
+            return matchesFilter(d, filter);
+        }).length;
+
+        const filteredData = data.filter(d => {
+            return !matchesFilter(d, filter);
+        });
+
+        this.data.set(table, filteredData);
+
+        return toDelete;
     }
 
     /**
@@ -159,8 +429,16 @@ export class TemplateDriver implements DataSourceDriver {
      * @param id Name of the primary key
      * @param field Name of the field to aggregate
      */
-    sum(table: string, filter: GenericFilter, id: string, field: string): Promise<number> {
-        throw new Error("Method not implemented.");
+    public async sum(table: string, filter: GenericFilter, id: string, field: string): Promise<number> {
+        const data = this.getTable(table);
+
+        const filteredData = data.filter(d => {
+            return matchesFilter(d, filter);
+        });
+
+        return filteredData.reduce((prev: number, row: any) => {
+            return prev + row[field];
+        }, 0);
     }
 
     /**
@@ -171,7 +449,15 @@ export class TemplateDriver implements DataSourceDriver {
      * @param prop The field to increment
      * @param inc The amount to increment
      */
-    increment(table: string, keyName: string, keyValue: GenericKeyValue, prop: string, inc: number): Promise<void> {
-        throw new Error("Method not implemented.");
+    public async increment(table: string, keyName: string, keyValue: GenericKeyValue, prop: string, inc: number): Promise<void> {
+        const data = this.getTable(table);
+
+        const filteredData = data.filter(d => {
+            return d[keyName] === keyValue;
+        });
+
+        filteredData.forEach(d => {
+            d[prop] += inc;
+        });
     }
 }
